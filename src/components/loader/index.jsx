cf5092e4b248e2463a191edfbe7b5a708a9fc1d7
abhishekrawatwebdev <abhishekrawat9956088862@gmail.com>
2022-12-25 04:06:59 +0530

    removed

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './loader.css'
 const Loader = () => {
     let index = 0;
@@ -16,11 +16,11 @@ const Loader = () => {
             name:"Hola!"
         }
     ]
-    useEffect(()=>{
-        setInterval(()=>{
-            index=index+1;
-        },1000)
-    },[index])
+    // useEffect(()=>{
+    //     setInterval(()=>{
+    //         index=index+1;
+    //     },1000)
+    // },[index])
   return (
     <div className='loader'>
        <h3> {loaderData[index].name}</h3>
