91ae784ee6f6a1a6424dbc18e98ea16eb22ff7e4
abhishekrawatwebdev <abhishekrawat9956088862@gmail.com>
2023-01-11 04:48:18 +0530

    added manifest and services

--- src/sections/tech_sections/index.jsx
+++ src/sections/tech_sections/index.jsx
@@ -6,7 +6,7 @@ const TechSection = () => {
   return (
     <div className='tech-section'>
       <div>
-        <h2 className="section-heading techs-heading section-top-heading" data-aos="fade-up" >Tools</h2>
+        <h2 className="section-heading techs-heading section-top-heading" data-aos="fade-up" >Techs</h2>
         <p className='tech-desc'  data-aos="fade-up" >These are the  technologies that I  use to develop scalable <br />
 frontend applications which are easy to maintain.
         </p>
